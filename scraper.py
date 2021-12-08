9831a167eebd6c0bbdf7dfd20ee5ed48f1ea34e5
YB96 <bhairaoyash@gmail.com>
2021-12-08 12:16:42 +0530

    defined a function get_videos

--- scraper.py
+++ scraper.py
@@ -1,11 +1,23 @@
+import smtplib
+import os
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-youtube_url = 'https://www.youtube.com/feed/trending'
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('D:\python\chromedriver.exe',options=chrome_options)
-driver.get(youtube_url)
-print('page title',driver.title)
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome('D:\python\chromedriver.exe',options=chrome_options)
+    return driver
+drive = driver()
+
+def get_videos(driver):
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  driver.get(YOUTUBE_TRENDING_URL)
+  videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+  return videos
